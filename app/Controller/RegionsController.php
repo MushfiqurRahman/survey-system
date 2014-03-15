b2eacc5c7af8a1260b6289074e9e710da218acfe
MushfiqurRahman <mushfique_sust@yahoo.com>
2014-03-15 20:09:05 +0600

    Import is almost done

--- app/Controller/RegionsController.php
+++ app/Controller/RegionsController.php
@@ -14,6 +14,8 @@ class RegionsController extends AppController {
  * @var array
  */
 	public $components = array('Paginator');
+        
+        var $regions, $territories, $towns, $outlets, $outletTypes;
 
 /**
  * index method
@@ -103,6 +105,177 @@ class RegionsController extends AppController {
 	}
         
         public function import_universe(){
+            if( $this->request->is('post') ){
+                if( !empty($this->request->data['Region']['xls_file']) ){
+                    //pr($this->data);exit;
+                    if( $this->request->data['Region']['xls_file']['error']==0){
+                        $renamed_f_name = time().$this->request->data['Region']['xls_file']['name'];
+                        if( move_uploaded_file($this->request->data['Region']['xls_file']['tmp_name'], WWW_ROOT.$renamed_f_name) ){
+                        	
+                            if( $this->_import($renamed_f_name) ){
+                                
+                                if( $this->Region->Territory->Town->Outlet->saveMany($this->outlets) ){
+                                    $this->Session->setFlash(__('Data import successful.'));
+                                }else{
+                                    $this->Session->setFlash(__('Data saving failed!'));
+                                }
+                            }else{
+                                $this->Session->setFlash(__('Data import failed!'));
+                            }
+                        }else{
+                            $this->Session->setFlash(__('File upload failed! Please try again.'));
+                        }
+                    }else{
+                        $this->Session->setFlash(__('Your given file is corrupted! Please try with valid file.'));
+                    }
+                }else{
+                    $this->Session->setFlash(__('You have not selected any file to upload.'));
+                }
+            }
+        }
+        
+        /**
+         * 
+         */
+        protected function _import( $xlName ){
+            
+            ini_set('memory_limit','512M');
+            set_time_limit(600);
+            
+            $this->regions = $this->territories = $this->towns = $this->outlets = array();
+            
+            $startTime = microtime(true);
+            
+            App::import('Vendor','PHPExcel',array('file' => 'PHPExcel/Classes/PHPExcel.php'));
+            
+            //here i used microsoft excel 2007
+            $objReader = PHPExcel_IOFactory::createReader('Excel2007');
+            //set to read only
+            $objReader->setReadDataOnly(true);
+            //load excel file
+            $objPHPExcel = $objReader->load($xlName);
+            $objWorksheet = $objPHPExcel->setActiveSheetIndex(0);
+            
+            $totalRow = $objPHPExcel->getActiveSheet()->getHighestRow();
+            
+            //echo $totalRow; exit;
+            
+            //for($i=2; $i<=$totalRow; $i++){                
+            for($i=5; $i<=$totalRow; $i++){    
+                
+                $this->outlets[$i-5]['Outlet'] = array();
+                
+                for($j=1;$j<10;$j++){
+                    
+                    switch($j){
+                        case 1:
+                            $regionId = $this->_save_region(strtolower(trim( $objWorksheet->getCellByColumnAndRow(1,$i)->getValue())));
+                            break;
+                        
+                        case 2:
+                            $territoryId = $this->_save_territory(strtolower(trim( $objWorksheet->getCellByColumnAndRow(1,$i)->getValue())), $regionId);
+                            break;
+                        
+                        case 3:
+                            $this->outlets[$i-5]['Outlet']['town_id'] = 
+                                $this->_save_town(strtolower(trim( $objWorksheet->getCellByColumnAndRow(1,$i)->getValue())), $territoryId);
+                            break;
+                        
+                        case 4:
+                            $this->outlets[$i-5]['Outlet']['name'] = 
+                                strtolower(trim($objWorksheet->getCellByColumnAndRow(1,$i)->getValue()));
+                            break;
+                        
+                        case 5:
+                            $this->outlets[$i-5]['Outlet']['address'] = 
+                                strtolower(trim($objWorksheet->getCellByColumnAndRow(1,$i)->getValue()));
+                            break;
+                        
+                        case 6:
+                            $this->outlets[$i-5]['Outlet']['outlet_type_id'] = 
+                                $this->_outlet_type_id(strtolower(trim($objWorksheet->getCellByColumnAndRow(1,$i)->getValue())));
+                            break;
+                        
+                        case 7:
+                            $this->outlets[$i-5]['Outlet']['phone'] = 
+                                strtolower(trim($objWorksheet->getCellByColumnAndRow(1,$i)->getValue()));
+                            break;
+                        
+                        case 8:
+                            $this->outlets[$i-5]['Outlet']['class'] = 
+                                strtolower(trim($objWorksheet->getCellByColumnAndRow(1,$i)->getValue()));
+                            break;
+                        
+                        case 9:
+                            $this->outlets[$i-5]['Outlet']['dms_code'] = 
+                                strtolower(trim($objWorksheet->getCellByColumnAndRow(1,$i)->getValue()));
+                            break;
+                    }
+                }             
+            }
+            $endTime = microtime(true);
+            echo 'Total spent time: '.($endTime - $startTime);
+            return true;
+        }
+        
+        
+        
+        /**
+         * 
+         * Enter description here ...
+         * @param unknown_type $region
+         */
+        protected function _save_region( $region ){            
+            foreach($this->regions as $rg){
+                if( $rg == $region ){
+                    return $this->regions[$region];//contains the region id
+                }
+            }            
+            $region['Region']['title'] = $region;
+            $this->Region->create();
+            $this->Region->save($region);
+            $this->regions[$region] = $this->Region->id;
+            return $this->Region->id;
+        }
+        
+        /**
+         * 
+         * Enter description here ...
+         * @param unknown_type $area
+         */
+	protected function _save_territory( $territory, $regionId ){
+            foreach($this->territories as $tr){
+                if( $tr == $territory ){
+                    return $this->territories[$territory];//contains the territory id
+                }
+            }            
+            $Trtr['Territory']['title'] = $region;
+            $Trtr['Territory']['region_id'] = $regionId;
+            $this->Region->Territory->create();
+            $this->Region->Territory->save($Trtr);
+            $this->territories[$territory] = $this->Region->Territory->id;
+            return $this->Region->Territory->id;
+        }
+        
+        /**
+         * 
+         * Enter description here ...
+         */
+	protected function _save_town( $town, $territoryId ){
+            foreach($this->towns as $tw){
+                if( $tw == $town ){
+                    return $this->towns[$tw];//contains the town id
+                }
+            }            
+            $Twn['Town']['title'] = $region;
+            $Twn['Town']['territory_id'] = $regionId;
+            $this->Region->Territory->Town->create();
+            $this->Region->Territory->Town->save($Twn);
+            $this->towns[$town] = $this->Region->Territory->Town->id;
+            return $this->Region->Territory->Town->id;
+        }
+        
+        protected function _save_outlet_type($type){
             
         }
 }
--- app/View/Regions/import_universe.ctp
+++ app/View/Regions/import_universe.ctp
@@ -1,19 +1,7 @@
-<div class="regions form">
-<?php echo $this->Form->create('Region'); ?>
-	<fieldset>
-		<legend><?php echo __('Add Region'); ?></legend>
-	<?php
-		echo $this->Form->input('title');
-	?>
-	</fieldset>
-<?php echo $this->Form->end(__('Submit')); ?>
-</div>
-<div class="actions">
-	<h3><?php echo __('Actions'); ?></h3>
-	<ul>
-
-		<li><?php echo $this->Html->link(__('List Regions'), array('action' => 'index')); ?></li>
-		<li><?php echo $this->Html->link(__('List Territories'), array('controller' => 'territories', 'action' => 'index')); ?> </li>
-		<li><?php echo $this->Html->link(__('New Territory'), array('controller' => 'territories', 'action' => 'add')); ?> </li>
-	</ul>
-</div>
+<h1><?php echo __('Import Universe data');?></h1>
+<?php
+    echo $this->Form->create('Region',array('type' => 'file'));
+    echo $this->Form->input('xls_file',array('type' => 'file','label' => 'Select the universe file'));
+    
+    echo $this->Form->end('Import');
+?>
